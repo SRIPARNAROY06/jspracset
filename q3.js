016bc845233eb028f26dc9c5407a8ffbd47ba2b0
SRIPARNA ROY <sriparna.roy3640@gmail.com>
2022-09-26 19:57:32 +0530

    jspracsetcommit2

--- q3.js
+++ q3.js
@@ -1,7 +1,7 @@
 let arr1=["CSE","ECE","CSE","IT"];
 let arr2=["Tulsi","Subhradeep","Suvam","Akash"];
 let merge= arr1.map(function(value,index){
-    return ["stream:" + value, "name:" +arr2[index]]
+    return {stream:value, name:arr2[index]}
 });
 console.log(merge);
 
